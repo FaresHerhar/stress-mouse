fbc91f0fe60744a2d937ed3583628e06cfe12b79
Herhar Fares <herhar.fares@yandex.com>
2020-03-23 22:09:54 +0100

    Mon 23 Mar 22:09:54 CET 2020

--- __init__.py
+++ __init__.py
@@ -1,7 +1,28 @@
-from autoclicker import *
+import keyboard
 
-if __name__ == "__main__":
-    # time laping, it should be in seconds
-    time_lap = 2
-    clicker = AutoClicker(time_lap)
-    clicker.run()
+START = "Ctrl + Shift + F5"         # starting keyboard shortcut
+STOP = "Ctrl + Shift + F6"         # pausing keyboard shortcut
+PAUSE = "Ctrl + Shift + F7"          # stoping keyboard shortcut
+
+
+def start():
+    print("STARTING")
+
+
+def stop():
+    print("STOPING")
+
+
+def pause():
+    while not keyboard.is_pressed("l"):
+        print("PAUSING")
+
+
+keyboard.add_hotkey(START, start)
+keyboard.add_hotkey(STOP, stop)
+keyboard.add_hotkey(PAUSE, pause)
+
+was = False
+
+while not keyboard.is_pressed("ESC"):
+    pass
--- autoclicker.py
+++ autoclicker.py
@@ -3,21 +3,75 @@ import time
 
 
 class AutoClicker:
-    def __init__(self, time_lap, pos_x=None, pos_y=None):
-        self.time_lap = time_lap
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+    """The AutoClicker class, defines the autoclicking
+    programm on the mouse, used the pyautogui and time library.
 
-    def run(self):
-        count = 0
+    Attributes:
+        time_lap, pos_x, pos_y, clicks, button, times.
 
+    Methodes:
+        __init__
+        click
+        setMousePosition
+        runEnd
+        runEndless
+    """
+
+    def __init__(self, time_lap, pos_x=None, pos_y=None, clicks=1,
+                 button=pyautogui.LEFT, times=None):
+        """This is the initialisation method:
+        the definition of the variables are below.
+
+        self.time_lap = time_lap    # the time lap, between each click
+        self.pos_x = pos_x          # the x posistion of the mouse
+        self.pos_y = pos_y          # the y posistion of the mouse
+        self.clicks = clicks        # the number of clicke, per time
+        self.button = button        # the clicked button, either left or right
+        self.times = times          # how many times, to do the clicks
+
+        time_lap, is in Secondes
+        self.pos_x and self.pos_y, are integers but None by default
+                                    for current curssor position
+        self.clicks, integer it takes 1 by default, but in general it takes (1,2)
+        self.times, integer but None by default for no stop clicking.
+        """
+        self.time_lap = time_lap    # the time lap, between each click
+        self.pos_x = pos_x          # the x posistion of the mouse
+        self.pos_y = pos_y          # the y posistion of the mouse
+        self.clicks = clicks        # the number of clicke, per time
+        self.button = button        # the clicked button, either left or right
+        self.times = times          # how many times, to do the clicks
+
+    def click(self):
+        """This method defines a single click of the mouse."""
+        pyautogui.click(clicks=self.clicks, button=self.button)
+
+    def setMousePosition(self):
+        """This method, defines the mouse's cursor position.
+        In case the user doesn't specify the positions,
+        use the default position, of where the mouse is.
+        """
         if self.pos_x is None and self.pos_y is None:
             self.pos_x, self.pos_y = pyautogui.position()
 
+    def runEndless(self):
+        """FOR ENDLESS ITERATIONS, UNKOWN TIMES OF ITERATIONS.
+        This is the run method it defines the all programm.
+        from clicking, to time sleeping.
+        """
+        self.setMousePosition()             # setting the possition
+
         while True:
-            pyautogui.click(clicks=1)
+            self.click()                    # on click event
+            time.sleep(self.time_lap)       # sleeping
 
-            print("Click :::", count)
-            count += 1
+    def runEnd(self):
+        """FOR A KNOWN NUMBER OF TIMES.
+        This is the run method it defines the all programm.
+        from clicking, to time sleeping.
+        """
+        self.setMousePosition()             # setting the possition
 
-            time.sleep(self.time_lap)
+        for count in range(self.times):
+            self.click()                    # on click event
+            time.sleep(self.time_lap)       # sleeping
