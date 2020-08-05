c470e3c6b931010eb7f4481966ea94ca77eb4b58
Herhar Fares <herhar.fares@yandex.com>
2020-08-05 21:37:14 +0100

    tests

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from autoclicker import AutoClicker
 # declaring, see autoclicker.py and read the documentation
 clicker = AutoClicker(time_lap=2, times=10)
 clicker = AutoClicker(time_lap=10, times=None, clicks=1, button='right',
-                      pox_x=123, pox_y=444)
+                      pos_x=123, pos_y=444)
 clicker = AutoClicker(clicks=2, button='left')
 
 # start the autoclicker
